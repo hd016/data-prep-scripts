54a3ec0a2192502f2c9fd93e418be22c7b874020
Harun Dalici <hd016@hdm-stuttgart.de>
2017-06-01 13:11:02 +0200

    gui 0.1 + self commands
    gui 0.1 + self commands

--- web-scrapper.py
+++ web-scrapper.py
@@ -1,7 +1,40 @@
 ##### webscrapper
 
+from tkinter import *
+
+##### basic_url ####
 basic_url = "https://www.gelbeseiten.de/arzt/stuttgart/s"
-iterator_page = 20
-for iterator_page in range(21):
-    multiple_url = "".join([basic_url,str(iterator_page)])
-    print (multiple_url)
+iterator_page = 0
+multiple_url = []
+for iterator_page in range(20):
+    iterator_page = iterator_page + 1
+    multiple_url.append("".join([basic_url,str(iterator_page),"\n"]))
+##### basic_url ####
+
+
+class App:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.button = Button(frame, text="SUCHE", fg ="blue", command=self.write_slogan)
+        self.button.pack(side=LEFT)
+
+    def write_slogan(self):
+        print ("Suchanfrage angenommen")
+        self.wall_output = Label(root,
+          compound = CENTER,
+          text=multiple_url)
+        self.wall_output.pack()
+      
+
+root = Tk()
+app = App(root)
+
+entry = Entry(root)
+
+wall_label = Label(root, text="Gelbe Seiten durchsuchen")
+
+wall_label.pack()
+entry.pack()
+
+root.mainloop()
