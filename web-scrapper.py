e35a9674bb2ba419b256fedbd11782ea9d60befc
Harun Dalici <hd016@hdm-stuttgart.de>
2017-06-06 16:55:48 +0200

    beta2

--- web-scrapper.py
+++ web-scrapper.py
@@ -3,57 +3,62 @@
 from tkinter import *
 from bs4 import BeautifulSoup
 import urllib.request
-import html2text
-
-
+from prettytable import PrettyTable
+import csv
 
 class App:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        self.button = Button(frame, text="SUCHE", fg ="blue", command=self.search)
+        self.button = Button(frame, text="SUCHE", fg="blue", command=self.search)
         self.button.pack(side=LEFT)
         self.entry = Entry(root)
         self.entry.pack()
 
     def search(self):
         self.entry_get = self.entry.get()
-        print ("Suchanfrage angenommen:", self.entry_get)
-    
+        print("Suchanfrage angenommen:", self.entry_get)
+
         self.sub_url = "https://www.gelbeseiten.de/{}/stuttgart/s".format(self.entry_get)
-        
+
         iterator_page = 0
         multiple_url = []
         for iterator_page in range(20):
             iterator_page = iterator_page + 1
-            multiple_url.append("".join([self.sub_url,str(iterator_page)]))
-            
-        #self.wall_output = Label(root,compound = CENTER,text=multiple_url)
-        #self.wall_output.pack()
+            multiple_url.append("".join([self.sub_url, str(iterator_page)]))
+
+        # self.wall_output = Label(root,compound = CENTER,text=multiple_url)
+        # self.wall_output.pack()
 
         parser = 0
-        #while parser < len(multiple_url):
-          #  print(multiple_url[parser])
-            #parser += 1
+        # while parser < len(multiple_url):
+        #  print(multiple_url[parser])
+        # parser += 1
 
-        text_file = open("Output.txt" , "w")
+        text_file = open("Output3.txt", "w")
+        csv_file = open("test.csv", "w")
+        table = PrettyTable(['Namen', 'Adressen'])
+
         with urllib.request.urlopen("file:///C:/Users/HDALICI/Desktop/test.html") as url:
             soup = BeautifulSoup(url, "html.parser")
 
-            names = [name.get_text() for name in soup.findAll("div", { "class" : "name m08_name" })]
-            addresses = [address.get_text() for address in soup.findAll("div", { "class" : "adresse m08_adresse" })]
-            for line in zip(names, addresses):
-                #print("%s\n%s" % line)
-                text_file.write("--------------")
-                text_file.write("%s%s" % line)
-            text_file.close()
-            print("Prozess abgeschlossen")
+            names = [name.get_text().strip() for name in soup.findAll("div", {"class": "name m08_name"})]
+            street = [address.get_text().strip() for address in soup.findAll(itemprop="streetAddress")]
+            plz = [address.get_text().strip() for address in soup.findAll(itemprop="postalCode")]
+            city = [address.get_text().strip() for address in soup.findAll(itemprop="addressLocality")]
 
-            
+            for line in zip(names, street, plz , city):
+                print("%s;%s;%s;%s;\n" % line)
+                text_file.write("%s;%s;%s;%s;\n" % line)
+
+
+            text_file.close()
+            print("Prozess finish")
+
 
 root = Tk()
 app = App(root)
-s
+
 wall_label = Label(root, text="Gelbe Seiten durchsuchen")
 
 wall_label.pack()
